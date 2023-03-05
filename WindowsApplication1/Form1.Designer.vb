df67856e2eb47185236a0e1994bf88d761c744b9
510208 <107909497+510208@users.noreply.github.com>
2023-03-05 11:26:30 +0800

    Add files via upload

--- WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class MainForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.Label1 = New System.Windows.Forms.Label
         Me.TextBox1 = New System.Windows.Forms.TextBox
         Me.Button1 = New System.Windows.Forms.Button
@@ -42,68 +43,47 @@ Partial Class MainForm
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel
         Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar
         Me.SavePGBW = New System.Windows.Forms.SaveFileDialog
+        Me.pnlTitleBar = New System.Windows.Forms.Panel
         Me.GroupBox1.SuspendLayout()
         Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        resources.ApplyResources(Me.Label1, "Label1")
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(169, 12)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "欲查詢的城市名稱(須為英文)："
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(23, 26)
+        resources.ApplyResources(Me.TextBox1, "TextBox1")
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(333, 22)
-        Me.TextBox1.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(362, 26)
+        resources.ApplyResources(Me.Button1, "Button1")
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(151, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "調用主程式查詢(建議)(&S)"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'OpenPGBW
         '
-        Me.OpenPGBW.Filter = "天氣暫存檔(*.pgbWeather)|*.pgbWeather|標準天氣暫存檔(Output.pgbWeather)|Output.pgbWeather|所有檔" & _
-            "案(*.*)|*.*"
-        Me.OpenPGBW.Title = "開啟天氣檔案"
+        resources.ApplyResources(Me.OpenPGBW, "OpenPGBW")
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(11, 33)
+        resources.ApplyResources(Me.Button2, "Button2")
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(89, 23)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "讀取檔案(&R)"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'pgbwPathShow
         '
-        Me.pgbwPathShow.AutoSize = True
-        Me.pgbwPathShow.Location = New System.Drawing.Point(106, 39)
+        resources.ApplyResources(Me.pgbwPathShow, "pgbwPathShow")
         Me.pgbwPathShow.Name = "pgbwPathShow"
-        Me.pgbwPathShow.Size = New System.Drawing.Size(68, 12)
-        Me.pgbwPathShow.TabIndex = 5
-        Me.pgbwPathShow.Text = "路徑：(N/A)"
         '
         'LinkLabel1
         '
-        Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Location = New System.Drawing.Point(366, 240)
+        resources.ApplyResources(Me.LinkLabel1, "LinkLabel1")
         Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(137, 12)
-        Me.LinkLabel1.TabIndex = 6
         Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "軟體由企鵝哥510208設計"
         '
         'GroupBox1
         '
@@ -114,107 +94,81 @@ Partial Class MainForm
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Button2)
         Me.GroupBox1.Controls.Add(Me.pgbwPathShow)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 55)
+        resources.ApplyResources(Me.GroupBox1, "GroupBox1")
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(491, 182)
-        Me.GroupBox1.TabIndex = 7
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "查詢"
         '
         'ProgressBar1
         '
         Me.ProgressBar1.ForeColor = System.Drawing.Color.LawnGreen
-        Me.ProgressBar1.Location = New System.Drawing.Point(11, 56)
+        resources.ApplyResources(Me.ProgressBar1, "ProgressBar1")
         Me.ProgressBar1.Maximum = 10
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(474, 12)
-        Me.ProgressBar1.TabIndex = 9
         '
         'weatherInfoLbl
         '
-        Me.weatherInfoLbl.AutoSize = True
+        resources.ApplyResources(Me.weatherInfoLbl, "weatherInfoLbl")
         Me.weatherInfoLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.weatherInfoLbl.Font = New System.Drawing.Font("微软雅黑", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.weatherInfoLbl.Location = New System.Drawing.Point(137, 86)
         Me.weatherInfoLbl.Name = "weatherInfoLbl"
-        Me.weatherInfoLbl.Size = New System.Drawing.Size(81, 22)
-        Me.weatherInfoLbl.TabIndex = 8
-        Me.weatherInfoLbl.Text = "(請先選擇)"
         '
         'Label3
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(9, 71)
+        resources.ApplyResources(Me.Label3, "Label3")
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(65, 12)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "查詢項目："
         '
         'weatherInfoSel
         '
-        Me.weatherInfoSel.Enabled = False
+        resources.ApplyResources(Me.weatherInfoSel, "weatherInfoSel")
         Me.weatherInfoSel.FormattingEnabled = True
-        Me.weatherInfoSel.ItemHeight = 12
-        Me.weatherInfoSel.Items.AddRange(New Object() {"請求地點", "天氣概況", "氣溫", "最高溫", "最低溫", "體感溫度", "濕度", "風速", "風向", "雲度"})
-        Me.weatherInfoSel.Location = New System.Drawing.Point(11, 86)
+        Me.weatherInfoSel.Items.AddRange(New Object() {resources.GetString("weatherInfoSel.Items"), resources.GetString("weatherInfoSel.Items1"), resources.GetString("weatherInfoSel.Items2"), resources.GetString("weatherInfoSel.Items3"), resources.GetString("weatherInfoSel.Items4"), resources.GetString("weatherInfoSel.Items5"), resources.GetString("weatherInfoSel.Items6"), resources.GetString("weatherInfoSel.Items7"), resources.GetString("weatherInfoSel.Items8"), resources.GetString("weatherInfoSel.Items9")})
         Me.weatherInfoSel.Name = "weatherInfoSel"
-        Me.weatherInfoSel.Size = New System.Drawing.Size(120, 88)
-        Me.weatherInfoSel.TabIndex = 6
         '
         'Label2
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(9, 18)
+        resources.ApplyResources(Me.Label2, "Label2")
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(65, 12)
-        Me.Label2.TabIndex = 5
-        Me.Label2.Text = "查詢結果："
         '
         'NotifyIcon1
         '
-        Me.NotifyIcon1.Text = "NotifyIcon1"
-        Me.NotifyIcon1.Visible = True
+        resources.ApplyResources(Me.NotifyIcon1, "NotifyIcon1")
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(11, 240)
+        resources.ApplyResources(Me.Button3, "Button3")
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 19)
-        Me.Button3.TabIndex = 10
-        Me.Button3.Text = "常駐(&F)"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'StatusStrip1
         '
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 266)
+        resources.ApplyResources(Me.StatusStrip1, "StatusStrip1")
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(515, 22)
-        Me.StatusStrip1.TabIndex = 11
-        Me.StatusStrip1.Text = "StatusStrip1"
         '
         'ToolStripStatusLabel1
         '
         Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(67, 17)
-        Me.ToolStripStatusLabel1.Text = "讀檔狀態："
+        resources.ApplyResources(Me.ToolStripStatusLabel1, "ToolStripStatusLabel1")
         '
         'ToolStripProgressBar1
         '
         Me.ToolStripProgressBar1.Maximum = 10
         Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
-        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        resources.ApplyResources(Me.ToolStripProgressBar1, "ToolStripProgressBar1")
         '
         'SavePGBW
         '
-        Me.SavePGBW.Filter = "企鵝哥天氣檔案(*.pgbWeather)|*.pgbWeather|所有檔案(*.*)|*.*"
-        Me.SavePGBW.Title = "儲存天氣檔案"
+        resources.ApplyResources(Me.SavePGBW, "SavePGBW")
+        '
+        'pnlTitleBar
+        '
+        resources.ApplyResources(Me.pnlTitleBar, "pnlTitleBar")
+        Me.pnlTitleBar.Name = "pnlTitleBar"
         '
         'MainForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(515, 288)
+        Me.Controls.Add(Me.pnlTitleBar)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.GroupBox1)
@@ -222,8 +176,8 @@ Partial Class MainForm
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "MainForm"
-        Me.Text = "WeatherLookup"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.StatusStrip1.ResumeLayout(False)
@@ -251,5 +205,6 @@ Partial Class MainForm
     Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
     Friend WithEvents ToolStripProgressBar1 As System.Windows.Forms.ToolStripProgressBar
     Friend WithEvents SavePGBW As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents pnlTitleBar As System.Windows.Forms.Panel
 
 End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,6 +3,7 @@
 Public Class MainForm
     Dim pgbwPath As String
     Dim weatherInfo(9)
+    Dim nOldWndLeft, nOldWndTop, nClickX, nClickY
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim myPath As String
@@ -113,4 +114,23 @@ error_Proc:
     Function cToK(ByVal C As Long)
         cToK = Val(C) + 273
     End Function
+
+    Private Sub pnlTitleBar_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlTitleBar.MouseDown
+        '紀錄滑鼠點選時的視窗位置與滑鼠點選位置
+        nOldWndLeft = Me.Left
+        nOldWndTop = Me.Top
+        nClickX = e.X
+        nClickY = e.Y
+    End Sub
+
+    Private Sub pnlTitleBar_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlTitleBar.MouseMove
+        If pnlTitleBar.Capture = True Then      '如果滑鼠按著拖曳
+            '設定新的視窗位置
+            Me.Top = e.Y + nOldWndTop - nClickY
+            Me.Left = e.X + nOldWndLeft - nClickX
+            '更新紀錄的視窗位置
+            nOldWndLeft = Me.Left
+            nOldWndTop = Me.Top
+        End If
+    End Sub
 End Class
